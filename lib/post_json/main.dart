fab4b7d53dae7e0d25d715af4e220a09ae0307dc
Javlonbek <mutahharramazon@gmail.com>
2023-08-08 15:42:15 -0700

    Get all posts by user id add

--- lib/post_json/main.dart
+++ lib/post_json/main.dart
@@ -4,8 +4,7 @@ import 'package:http/http.dart';
 import 'package:post_json/post_json/models.dart';
 
 Future<void> main() async {
-  Uri getAllPost = Uri.parse(
-      'https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,userId');
+  Uri getAllPost = Uri.parse('https://dummyjson.com/posts/user/5');
   Response response = await get(getAllPost);
 
   Post post = Post.fromMap(jsonDecode(response.body));
