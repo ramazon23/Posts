d1f01b9aac9866700bc7e07df682f9c9e7b0f13f
Javlonbek <mutahharramazon@gmail.com>
2023-08-08 16:25:00 -0700

    Final realeased

--- lib/post_json/main.dart
+++ lib/post_json/main.dart
@@ -4,9 +4,9 @@ import 'package:http/http.dart';
 import 'package:post_json/post_json/models.dart';
 
 Future<void> main() async {
-  Uri getAllPost = Uri.parse('https://dummyjson.com/posts/add');
+  Uri getAllPost = Uri.parse('https://dummyjson.com/posts');
   Response response = await get(getAllPost);
 
-  Post post = Post.fromMap(jsonDecode(response.body));
-  print(post.posts[1]);
+  Posts posts = Posts.fromMap(jsonDecode(response.body));
+  print(posts);
 }
