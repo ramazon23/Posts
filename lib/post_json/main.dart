4e48d18f71c8a4ad3e09feb919f1d005813ba63b
Javlonbek <mutahharramazon@gmail.com>
2023-08-08 16:08:50 -0700

    Gitga chiqdi

--- lib/post_json/main.dart
+++ lib/post_json/main.dart
@@ -4,9 +4,9 @@ import 'package:http/http.dart';
 import 'package:post_json/post_json/models.dart';
 
 Future<void> main() async {
-  Uri getAllPost = Uri.parse('https://dummyjson.com/posts/1/comments');
+  Uri getAllPost = Uri.parse('https://dummyjson.com/posts/add');
   Response response = await get(getAllPost);
 
   Post post = Post.fromMap(jsonDecode(response.body));
-  print(post.posts);
+  print(post.posts[1]);
 }
