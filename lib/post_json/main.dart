b05fcbd1228bea7449f2335019bca2a117908c43
Javlonbek <mutahharramazon@gmail.com>
2023-08-08 15:38:53 -0700

    Serach qoshildi

--- lib/post_json/main.dart
+++ lib/post_json/main.dart
@@ -4,9 +4,9 @@ import 'package:http/http.dart';
 import 'package:post_json/post_json/models.dart';
 
 Future<void> main() async {
-  Uri getAllPost = Uri.parse('https://dummyjson.com/posts');
+  Uri getAllPost = Uri.parse('https://dummyjson.com/posts/search?q=love');
   Response response = await get(getAllPost);
 
   Post post = Post.fromMap(jsonDecode(response.body));
-  print(post.posts[0].body);
+  print(post.posts);
 }
